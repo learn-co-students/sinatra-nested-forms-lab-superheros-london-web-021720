676d10ca3ac8a3612f0aea460c563e42d81ead8c
Amaterasus <skyetay03@googlemail.com>
2020-03-10 01:45:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        details = params
+        @team = details["team"]
+        @members = @team["members"]
+        puts @members
+        erb :team
+    end
+
 
 end
